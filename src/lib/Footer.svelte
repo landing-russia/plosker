c3160f5551222cf67d59a1747b5417f51c79b273
landing-russia <landing-russia@yandex.ru>
2022-03-21 23:52:51 +0300

    add date in footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,16 @@
 <script>
   import Logo2 from "./Logo2.svelte";
+
+  $: currentYear = new Date().getFullYear();
 </script>
 
-<div class="bg-slate-900 h-16 w-full flex items-center justify-center text-gray-400 text-lg font-semibold shadow">
-  <Logo2 />
+<div
+  class="bg-slate-900 h-32 w-full flex items-center justify-center text-gray-400 text-lg font-semibold shadow"
+>
+  <div class="space-y-2 pt-2">
+    <Logo2 />
+    <p class="text-lg text-center text-slate-500 font-bitter font-normal">
+      &copy;&nbsp;{currentYear}
+    </p>
+  </div>
 </div>
