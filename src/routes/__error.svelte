812de4bc74a42b7a8f8adeed6d9b75e6ae57bcf2
landing-russia <landing-russia@yandex.ru>
2022-04-04 00:49:31 +0300

    404

---
+++ src/routes/__error.svelte
@@ -0,0 +1,8 @@
+<div class="mt-16 text-center">
+  <h1 class="mb-10 text-slate-100 text-3xl md:text-5xl font-semibold">
+    Не найдено
+  </h1>
+  <a href="/" class="text-slate-300 text-xl border-b border-slate-400"
+    >Перейти на главную</a
+  >
+</div>
