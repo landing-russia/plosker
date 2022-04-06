e34736e2a634ca6fb6910cf409eb475c0201ff18
landing-russia <landing-russia@yandex.ru>
2022-04-06 12:55:07 +0300

    404 travolta

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,8 +1,9 @@
-<div class="mt-16 text-center">
+<div class="mt-16 text-center flex flex-col items-center">
   <h1 class="mb-10 text-slate-100 text-3xl md:text-5xl font-semibold">
     Не найдено
   </h1>
   <a href="/" class="text-slate-300 text-xl border-b border-slate-400"
     >Перейти на главную</a
   >
+  <img class="mt-16" src="/travolta.gif" alt="Travolta">
 </div>
